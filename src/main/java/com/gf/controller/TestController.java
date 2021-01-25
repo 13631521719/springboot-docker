8071949c2f4bc49a5fbbf59b94d186d84726c760
13631521719 <598601307@qq.com>
2021-01-25 17:33:17 +0800

    修改端口

--- src/main/java/com/gf/controller/TestController.java
+++ src/main/java/com/gf/controller/TestController.java
@@ -1,14 +1,21 @@
 package com.gf.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class TestController {
+    @Value("${key1}")
+    private String key1;
+
+    @Value("${key2}")
+    private String key2;
 
     @RequestMapping(value = "/test")
     public @ResponseBody
@@ -16,6 +23,24 @@ public class TestController {
         Map map=new HashMap();
         map.put("code","200");
         map.put("message","欢迎你，我的主人，尊贵的刘林先生");
+        map.put("ip",getIp());
+        map.put("key1",key1);
+        map.put("key2",key2);
+        map.put("timestemp",System.currentTimeMillis());
         return map;
+    }
+
+
+    public static String getIp(){
+        try {
+            InetAddress ia= InetAddress.getLocalHost();
+//        String localname=ia.getHostName();
+            String localip=ia.getHostAddress();
+            return localip;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "500";
+        }
     }
 }
